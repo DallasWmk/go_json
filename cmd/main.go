8a960abb8453bd870a788b239a680296fe8e76a9
Dallas Womack <dallasawomack@gmail.com>
2025-07-10 13:15:26 -0700

    feat(go_json):add simple double quote check. Change validity checking

--- cmd/main.go
+++ cmd/main.go
@@ -8,32 +8,51 @@ import (
 )
 
 func main() {
-	file, err := os.Open("test.json")
+	validName := "test.json"
+	validFile, err := os.Open(validName)
 	if err != nil {
 		panic(err)
 	}
+	parse(validFile, validName)
+
+	invalidName := "invalidtest.json"
+	invalidFile, err := os.Open(invalidName)
+	if err != nil {
+		panic(err)
+	}
+	parse(invalidFile, invalidName)
+}
+
+func parse(file *os.File, fileName string) {
 	myLexer := lexer.NewLexer(file)
 	myMap := make(map[string]int)
+	validityCheck := map[string]bool{
+		"curly":   true,
+		"bracket": true,
+		"quote":   true,
+	}
 	for {
 		_, tok, _ := myLexer.Lex()
 		if tok == lexer.EOF {
 			break
 		}
 		myMap[tok.String()]++
+		switch tok.String() {
+		case `{`, `}`:
+			validityCheck["curly"] = !validityCheck["curly"]
+		case `[`, `]`:
+			validityCheck["bracket"] = !validityCheck["bracket"]
+		case `"`:
+			validityCheck["quote"] = !validityCheck["quote"]
+		default:
+			continue
+		}
 	}
-	if checkValidity(myMap) {
-		fmt.Println("JSON is valid")
-	} else {
-		fmt.Println("JSON is invalid")
+	for key, value := range validityCheck {
+		if !value {
+			fmt.Printf("Missing token: %s in %s\n", key, fileName)
+			return
+		}
 	}
-}
-
-func checkValidity(myMap map[string]int) bool {
-	if myMap[lexer.Tokens[lexer.CurlyOpen]] != myMap[lexer.Tokens[lexer.CurlyClose]] {
-		return false
-	}
-	if myMap[lexer.Tokens[lexer.BracketOpen]] != myMap[lexer.Tokens[lexer.BracketClose]] {
-		return false
-	}
-	return true
+	fmt.Printf("All tokens present in %s\n", fileName)
 }
